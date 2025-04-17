e9881764018cd5aac54577301b1932140105f455
Alik-Green <alikgreen@proton.me>
2025-04-17 17:41:01 +0100

    Stop tracking build artifacts

--- sandboxApp/cmake-build-debug/neonEngine/CMakeFiles/neonEngine.dir/src/rendering/window.cpp.obj.ddi.d
+++
@@ -1,3 +0,0 @@
-neonEngine\CMakeFiles\neonEngine.dir\src\rendering\window.cpp.obj.ddi: \
- C:/Users/alikg/CLionProjects/neonEngine/neonEngine/src/rendering/window.cpp \
- C:/Users/alikg/CLionProjects/neonEngine/neonEngine/src/rendering/window.h
