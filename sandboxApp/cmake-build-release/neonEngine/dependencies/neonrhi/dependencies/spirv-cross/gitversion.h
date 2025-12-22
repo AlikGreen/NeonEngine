eabc6b6c291e0291dc9b49a73870c4a97fe687f3
Alik Green <alikgreen8@gmail.com>
2025-12-22 19:48:35 +0000

    Stop tracking build output

--- sandboxApp/cmake-build-release/neonEngine/dependencies/neonrhi/dependencies/spirv-cross/gitversion.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-2021 The Khronos Group Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-#ifndef SPIRV_CROSS_GIT_VERSION_H_
-#define SPIRV_CROSS_GIT_VERSION_H_
-
-#define SPIRV_CROSS_GIT_REVISION "Git commit: vulkan-sdk-1.4.321.0-41-g0a88b2d5 Timestamp: 2025-12-21T14:10:01"
-
-#endif
