3a52bb83df7b577bcca4d3556ff2c0814275e3ea
Alik Green <alikgreen8@gmail.com>
2026-01-25 22:19:18 +0000

    i lied all along. d was good but ide support not good and people would have to learn a new language. so c# it is with a forked version of coral

---
+++ neonEngine/sdk/NeonManaged/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace NeonManaged;
+
+public class Class1
+{
+    internal static unsafe delegate*<void> InternalLogCall;
+
+    public static void CallBackToInternal(float value)
+    {
+        unsafe { InternalLogCall(); }
+    	Console.WriteLine(value);
+    }
+}
